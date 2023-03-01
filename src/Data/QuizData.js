24726627665bcb31c9454563f2626fdef05e4c93
Muskan Mittal <mittalmuskan758@gmail.com>
2023-03-01 13:23:45 +0530

    source code attached

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options: [ "Java", "C", "Python", "Javascript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [ "Central Style Sheets", "Cascading style sheets", "Central cascading sheets", "Javascript"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: [ "Hyper Text Markup Language", "C", "Python", "Javascript"],
+        answer: 1
+    },
+    {
+        question: "What year Javascript launched?",
+        options: [ "1996", "1995", "1994", "none of the above"],
+        answer: 4
+    },
+]
